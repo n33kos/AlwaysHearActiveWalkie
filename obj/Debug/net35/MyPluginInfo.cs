619901f3fe0fef5647d8bb627e6672c1dc33b83a
Nicholas Suski <n33kos@gmail.com>
2023-11-21 21:03:27 -0700

    Initial Commit With Template

---
+++ obj/Debug/net35/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace BepInEx5.PluginTemplate
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "BepInEx5.PluginTemplate";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
