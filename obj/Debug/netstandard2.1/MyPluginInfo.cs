54e8a2cdecc54362dbc52526d9107bf877413172
Nicholas <n33kos@gmail.com>
2023-11-22 16:32:07 -0700

    Version 1.1.0

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -4,6 +4,6 @@ namespace BepInEx5.PluginTemplate
     {
         public const string PLUGIN_GUID = "AlwaysHearWalkie";
         public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
--- thunderstore/CHANGELOG.md
+++ thunderstore/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.1.0
+- Make it actually work
+- Walking in and out of range while communication is in progress doesn't work
+
 # 1.0.0
 - Only listen to powered-on walkies
 - Limit distance at which users can hear walkied to 15
