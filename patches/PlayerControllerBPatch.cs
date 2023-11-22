38fefedcc875ac89173b590fe740b51970ffac90
Nicholas <n33kos@gmail.com>
2023-11-22 13:58:05 -0700

    Implement distance limitation

--- patches/PlayerControllerBPatch.cs
+++ patches/PlayerControllerBPatch.cs
@@ -1,4 +1,3 @@
-using System;
 using GameNetcodeStuff;
 using HarmonyLib;
 using UnityEngine;
@@ -8,11 +7,30 @@ namespace LCAlwaysHearWalkieMod.Patches
   [HarmonyPatch(typeof(PlayerControllerB))]
   internal class PlayerControllerBPatch
   {
-    [HarmonyPatch("Update")]
+    private static float AudibleDistance = 15f;
+
+    // Adds a postfix to the PlayerIsHearingOthersThroughWalkieTalkie method which returns true if the
+    // player is within range of a walkie talkie that is being used.
+    [HarmonyPatch("PlayerIsHearingOthersThroughWalkieTalkie")]
     [HarmonyPostfix]
-    static void alwaysHearWalkieTalkiesPatch(ref bool ___holdingWalkieTalkie)
+    static bool alwaysHearWalkieTalkiesPatch(bool result, ref PlayerControllerB __instance)
     {
-      ___holdingWalkieTalkie = true;
+      if (result == true) return true;
+
+      for (int i = 0; i < WalkieTalkie.allWalkieTalkies.Count; i++)
+      {
+        float distance = Vector3.Distance(WalkieTalkie.allWalkieTalkies[i].transform.position, __instance.transform.position);
+
+        if (
+          WalkieTalkie.allWalkieTalkies[i].isBeingUsed &&
+          distance <= AudibleDistance
+        )
+        {
+          return true;
+        }
+      }
+
+      return false;
     }
   }
 }
