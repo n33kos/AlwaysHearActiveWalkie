124c525531df3f0dae7310a36b80c8834abbbc38
Nicholas <n33kos@gmail.com>
2023-11-28 11:00:03 -0700

    Add build bat script

---
+++ build.bat
@@ -0,0 +1,3 @@
+dotnet build %~dp0AlwaysHearWalkie.csproj
+powershell -Command Copy-Item -Force -Path "%~dp0bin\Debug\netstandard2.1\AlwaysHearWalkie.dll" -Destination "%~dp0thunderstore\AlwaysHearWalkie.dll"
+powershell -Command Copy-Item -Force -Path "%~dp0bin\Debug\netstandard2.1\AlwaysHearWalkie.dll" -Destination """E:\SteamLibrary\steamapps\common\Lethal Company\BepInEx\plugins\AlwaysHearWalkie.dll"""
