842b75c7f9dd9bc3f461c38647b9466354c6746f
Nicholas <n33kos@gmail.com>
2023-11-28 10:24:57 -0700

    Delete build files and zips

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BepInEx5.PluginTemplate
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "AlwaysHearWalkie";
-        public const string PLUGIN_NAME = "Always Hear Active Walkies";
-        public const string PLUGIN_VERSION = "1.4.1";
-    }
-}
