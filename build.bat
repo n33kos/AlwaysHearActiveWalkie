93fc7b3105d5acf168c1ccb6d5d32061529237d4
Nicholas <n33kos@gmail.com>
2023-12-17 16:07:40 -0700

    Add start process command to build script

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 dotnet build %~dp0AlwaysHearWalkie.csproj
 powershell -Command Copy-Item -Force -Path "%~dp0bin\Debug\netstandard2.1\AlwaysHearWalkie.dll" -Destination "%~dp0thunderstore\AlwaysHearWalkie.dll"
 powershell -Command Copy-Item -Force -Path "%~dp0bin\Debug\netstandard2.1\AlwaysHearWalkie.dll" -Destination """E:\SteamLibrary\steamapps\common\Lethal Company\BepInEx\plugins\AlwaysHearWalkie.dll"""
+Start-Process & "E:\SteamLibrary\steamapps\common\Lethal Company\Lethal Company.exe"
